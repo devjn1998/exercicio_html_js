42884b384ccde99656f249623fe57772506d95b3
Juan Costa <juandev1998@gmail.com>
2024-03-11 15:33:00 -0300

    terceiro commit

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
 const form = document.querySelector('.inputConfig');
+const msgSucesso = document.querySelector('.msg-sucess');
+const msgFail = document.querySelector('.msg-fail');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const campoA = Number(document.getElementById('campoA').value);
     const campoB = Number(document.getElementById('campoB').value);
-    
+
     const mensagemSucess = `<p> Comparação Válida!! O valor <strong>${campoB}</strong> é maior que o <strong>${campoA}</strong>.</p>`
     const mensagemFail = `<p> Comparação Inválida!! O valor <strong>${campoA}</strong> é maior que o <strong>${campoB}</strong><br>Tente novamente..</p>`
 
-    const msgSucess = document.querySelector('.msg-sucess');
-    const msgFail = document.querySelector('.msg-fail');
 
     if (campoB > campoA) {
-        msgSucess.style.display = "block";
-        msgSucess.innerHTML = mensagemSucess;
-        msgSucess.classList.remove('msg-fail');
-        msgSucess.classList.add('msg-sucess');
-        
+        msgSucesso.style.display = "block";
+        msgSucesso.innerHTML = mensagemSucess;
+        msgSucesso.classList.remove('msg-fail');
+        msgSucesso.classList.add('msg-sucess');
+
         msgFail.style.display = "none";
-        
+
     }
     else {
-        msgFail.innerHTML = mensagemFail;
-        msgFail.style.display = "block";
-        msgSucess.style.display = "none";
-        msgSucess.classList.remove('msg-sucess');
-        msgSucess.classList.add('msg-fail');
-        
-        
+        if (msgFail.style.display !== "none") {
+            msgFail.innerHTML = mensagemFail;
+            msgFail.style.display = "block";
+            msgSucesso.style.display = "none";
+            msgSucesso.classList.remove('msg-sucess');
+            msgSucesso.classList.add('msg-fail');
+        }
+
+
+
     }
     document.getElementById('campoA').value = "";
     document.getElementById('campoB').value = "";
